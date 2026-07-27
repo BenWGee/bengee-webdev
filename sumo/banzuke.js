35650c515d646a913d5b2a3575c1dd47a6dcf8de
Bengee Steamdeck <bguilfoyle@protonmail.com>
2026-07-27 13:01:50 +0100

    work in progress banzuke page

---
+++ sumo/banzuke.js
@@ -0,0 +1,4 @@
+/*
+Script to control the contents of banzuke.html. It fetches the banzuke data from the API and populates the table with the relevant information. The script also handles user interactions, such as selecting a different division or basho, and updates the table accordingly.
+*/
+
